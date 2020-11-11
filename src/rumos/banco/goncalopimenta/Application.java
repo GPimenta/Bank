2626ace5e3369071f0a83bc59ade5f79ca87f76b
GoncaloPimenta <goncalo6pimenta@gmail.com>
2020-11-11 01:09:46 +0000

    Main class Application

---
+++ src/rumos/banco/goncalopimenta/Application.java
@@ -0,0 +1,13 @@
+package rumos.banco.goncalopimenta;
+
+import java.util.Scanner;
+
+public class Application {
+	public static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
