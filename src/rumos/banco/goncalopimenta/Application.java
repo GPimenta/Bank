a8be64781dd4a1484d8b9d413954b83215ed677f
GoncaloPimenta <goncalo6pimenta@gmail.com>
2020-11-14 18:27:04 +0000

    With autometic comments

--- src/rumos/banco/goncalopimenta/Application.java
+++ src/rumos/banco/goncalopimenta/Application.java
@@ -5,48 +5,73 @@ import java.util.Scanner;
 
 
 public class Application {
-	private static Customer[] customers = new Customer[3];
+	private static final int DATABASE_SIZE = 3;
+			
+	private static final int EXIT = 0;
+	private static final int CREATE_NEW_CUSTOMER = 1;
+	private static final int FIND_CUSTOMER_BY_NAME = 2;
+	private static final int FIND_CUSTOMER_BY_TAXID = 3;
+	private static final int SHOW_ALL_CUSTOMERS = 4;
+	private static final int EDIT_CUSTOMER_BY_ID = 5;
+	private static final int DELETE_CUSTOMER = 6;
+	
+
+
+	private static final String MOTD = "Welcome to Rumos Digital Bank";
+	private static final String GOODBYE = "Thanks for using Rumos Digital Bank";
+	private static final String CUSTOMER_CREATED = "Customer created!";
+	private static final String CUSTOMER_DELETED = "Customer deleted!";
+	private static final String CUSTOMER_NOT_FOUND = "Customer not found!";
+	private static final String DATABASE_IS_FULL = "Database is full!";
+	private static final String DATABASE_IS_EMPTY = "Database is empty!";
+	private static final String TAXID_ALREADY_EXISTS = "Customer Tax Id already exists!";
+	private static final String INVALID_OPTION = "Invalid Option!";
+	
+	
+	
+	
+	private static Customer[] customers = new Customer[DATABASE_SIZE];
 	private static Scanner scanner = new Scanner(System.in);
 	private static Integer option;
 
 
 	public static void main(String[] args) {
-
+		
 		do {
 			displayMenu();
 			option = scanner.nextInt();
 
 			switch (option) {
-			case 1:
+			case CREATE_NEW_CUSTOMER:
 				createNewCustomer();
 				break;
-			case 2:
+			case FIND_CUSTOMER_BY_NAME:
 				showCustomersByName();
 				//Show costumer by name
 
 				break;
-			case 3:
+			case FIND_CUSTOMER_BY_TAXID:
 				showCustomerByTaxId();
 				//Show costumer by taxId
 				break;
-			case 4:
+			case SHOW_ALL_CUSTOMERS:
 				showAllCustomers();
 				//Show all costumers
 				break;
-			case 5:
-				//Show customer
-				
-				break;
-			case 6:
+			case EDIT_CUSTOMER_BY_ID:
 				editCustomerByID();
 				//Edit customer by ID
+				
 				break;
-			case 7:
+			case DELETE_CUSTOMER:
 				deleteCustomerById();
 				//Delete customer by ID
 				break;
+			case EXIT:
+				System.out.println(GOODBYE);
+				break;
 			default:
-				System.err.println("Invalid option in DisplayMenu");
+				System.err.println(INVALID_OPTION + " in DisplayMenu");
 				break;
 			}
 		}while(option != 0);
@@ -58,10 +83,11 @@ public class Application {
 		for (int i = 0; i < customers.length; i++) {
 			if(customers[i].getId() == id){
 				customers[i] = null;
+				System.out.println(CUSTOMER_DELETED);
 				return;
 			}
 		}
-		System.out.println("Customer not found");
+		System.out.println(CUSTOMER_NOT_FOUND);
 	}
 
 	private static void editCustomerByID() {
@@ -99,7 +125,9 @@ public class Application {
 				customer.setEmail(scanner.next());
 				//change email
 				break;
-
+			case 0:
+				System.out.println("Returning to previous Menu");
+				break;
 			default:
 				System.err.println("Invalid option in EditMenu");
 				break;
@@ -127,7 +155,7 @@ public class Application {
 				return;
 			}
 		}
-		System.err.println("Name not found!");
+		System.err.println(CUSTOMER_NOT_FOUND);
 	}
 
 	private static void showAllCustomers() {
@@ -157,7 +185,7 @@ public class Application {
 		for(int i = 0; i < customers.length; i++  ) {
 			if(customers[i] == null) {
 				customers[i] = populateEmptyDatabase();
-				System.out.println("New customer created");
+				System.out.println(CUSTOMER_CREATED);
 				return;
 			}
 		}
@@ -165,24 +193,7 @@ public class Application {
 	}
 
 
-	private static void displayMenu() {
-		System.out.println("Welcome, from the list below please choose the action that u want take: ");
-		System.out.println("0 - Exit");
-		System.out.println("1 - Create new customer");
-		System.out.println("2 - Show customer by name");
-		System.out.println("3 - Show customer by taxId"); 
-		System.out.println("4 - Show all customers"); 
-		System.out.println("5 - Show customer");
-		System.out.println("6 - Edit customer by Id");
-		System.out.println("7 - Delete customer by Id");
-	}
-	private static void displayEditMenu() {
-		System.out.println("What do you which to edit?");
-		System.out.println("0 - Exit");
-		System.out.println("1 - Name of the customer");
-		System.out.println("2 - Password of the customer");
-		System.out.println("3 - Email of the customer"); 
-	}
+
 
 	private static Customer populateEmptyDatabase() {
 		System.out.println("Creating new client");
@@ -215,6 +226,24 @@ public class Application {
 		newCustomer.setBalance(scanner.nextDouble());
 
 		return newCustomer;
+	}
+	private static void displayMenu() {
+		System.out.println( MOTD +" please choose the action that u want take: ");
+		System.out.println("0 - Exit");
+		System.out.println("1 - Create new customer");
+		System.out.println("2 - Show customer by name");
+		System.out.println("3 - Show customer by taxId"); 
+		System.out.println("4 - Show all customers"); 
+		System.out.println("5 - Show customer");
+		System.out.println("6 - Edit customer by Id");
+		System.out.println("7 - Delete customer by Id");
+	}
+	private static void displayEditMenu() {
+		System.out.println("What do you which to edit?");
+		System.out.println("0 - Return to previous Menu");
+		System.out.println("1 - Name of the customer");
+		System.out.println("2 - Password of the customer");
+		System.out.println("3 - Email of the customer"); 
 	}
 
 }
