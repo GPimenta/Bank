f12b044cc4df330b338542490daef0da233888b6
GoncaloPimenta <goncalo6pimenta@gmail.com>
2020-12-11 14:52:48 +0000

    no message

---
+++ src/rumos/banco/model/Account.java
@@ -0,0 +1,38 @@
+package rumos.banco.model;
+
+import java.util.ArrayList;
+
+public class Account {
+
+	private String accountHolderNumber; // 5 digits
+	private Double accountHolderBalance;
+	private String[] secondaryAccountNumber = { "", "", "", "" };
+	private ArrayList<String> transactionHistory = new ArrayList<String>();
+	
+	
+	public String getAccountHolderNumber() {
+		return accountHolderNumber;
+	}
+	public void setAccountHolderNumber(String accountHolderNumber) {
+		this.accountHolderNumber = accountHolderNumber;
+	}
+	public Double getAccountHolderBalance() {
+		return accountHolderBalance;
+	}
+	public void setAccountHolderBalance(Double accountHolderBalance) {
+		this.accountHolderBalance = accountHolderBalance;
+	}
+	public String[] getSecondaryAccountNumber() {
+		return secondaryAccountNumber;
+	}
+	public void setSecondaryAccountNumber(String[] secondaryAccountNumber) {
+		this.secondaryAccountNumber = secondaryAccountNumber;
+	}
+	public ArrayList<String> getTransactionHistory() {
+		return transactionHistory;
+	}
+	public void setTransactionHistory(ArrayList<String> transactionHistory) {
+		this.transactionHistory = transactionHistory;
+	}
+	
+}
--- src/rumos/banco/model/Card.java
+++ src/rumos/banco/model/Card.java
@@ -1,5 +1,49 @@
 package rumos.banco.model;
 
 public class Card {
+	private Boolean debitCard = false;
+	private String debitCardNumber;
+	private String debitCardPin;
 
+	private Boolean creditCard = false;
+	private String creditCardNumber;
+	private String creditCardPin;
+	public Boolean getDebitCard() {
+		return debitCard;
+	}
+	public void setDebitCard(Boolean debitCard) {
+		this.debitCard = debitCard;
+	}
+	public String getDebitCardNumber() {
+		return debitCardNumber;
+	}
+	public void setDebitCardNumber(String debitCardNumber) {
+		this.debitCardNumber = debitCardNumber;
+	}
+	public String getDebitCardPin() {
+		return debitCardPin;
+	}
+	public void setDebitCardPin(String debitCardPin) {
+		this.debitCardPin = debitCardPin;
+	}
+	public Boolean getCreditCard() {
+		return creditCard;
+	}
+	public void setCreditCard(Boolean creditCard) {
+		this.creditCard = creditCard;
+	}
+	public String getCreditCardNumber() {
+		return creditCardNumber;
+	}
+	public void setCreditCardNumber(String creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+	public String getCreditCardPin() {
+		return creditCardPin;
+	}
+	public void setCreditCardPin(String creditCardPin) {
+		this.creditCardPin = creditCardPin;
+	}
+	
+	
 }
