b3ab32d5f535926b932f3e411dce065f519c1c1a
GoncaloPimenta <goncalo6pimenta@gmail.com>
2020-12-12 16:57:53 +0000

    no message

--- src/rumos/banco/service/CustomerService.java
+++ src/rumos/banco/service/CustomerService.java
@@ -1,5 +1,29 @@
 package rumos.banco.service;
 
+import rumos.banco.model.Customer;
+
 public class CustomerService {
+	Customer customer  = new Customer();
+	
+	public void editCustomerPersonalDetails() {
+		
+	}
+	
+	private  void editCustomerByNameAndPassword() {
+	
+	}
+	
+	private  void editCustomerByTaxID() {
+	
+	}
+	
+	private  void editCustomerByID() {
+		
+	}
+	
+	private Customer editCustomerDetails(Customer customer) {
+		return customer;
+		
+	}
 
 }
