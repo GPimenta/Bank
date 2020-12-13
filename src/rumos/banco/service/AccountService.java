3b0b0ce8a11bdf42206e92b21807581860608ba4
GoncaloPimenta <goncalo6pimenta@gmail.com>
2020-12-13 19:37:48 +0000

    no message

--- src/rumos/banco/service/AccountService.java
+++ src/rumos/banco/service/AccountService.java
@@ -8,63 +8,26 @@ import rumos.banco.model.Account;
 import rumos.banco.model.Customer;
 
 public class AccountService {
-	private final int EXIT = 0;
-	private final int CREATE_NEW_CUSTOMER = 1;
-	private final int SHOW_CUSTOMER = 2;
-	private final int EDIT_CUSTOMER_ACCOUNTS = 3;
-	private final int TO_DECIDE_2 = 4;
-	private final int EDIT_CUSTOMER_PERSONAL_DETAILS = 5;
-	private final int DELETE_CUSTOMER = 6;
-	private final int MANAGE_MONEY = 7;
-	private final int EDIT_BANK_CARDS = 8;
+	private static final int EXIT = 0;
+             
+             
+	private static final int DEPOSIT_MONEY_ON_HOLDER_ACCOUNT = 1;
+	private static final int DEPOSIT_MONEY_ON_SECUNDARY_ACCOUNT = 2;
+	private static final int WITHDRAW_MONEY_ON_HOLDER_ACCOUNT = 3;
+	private static final int WITHDRAW_MONEY_ON_SECUNDARY_ACCOUNT = 4;
+	private static final int TRANSFER_MONEY = 5;
+	private static final int CHECK_ACCOUNT_HISTORY = 6;
+             
 
-	private final int SHOW_CUSTOMER_BY_NAME = 1;
-	private final int SHOW_CUSTOMER_BY_TAXID = 2;
-	private final int SHOW_ALL_CUSTOMER = 3;
-
-	private final int EDIT_CUSTOMER_BY_NAME_AND_PASSWORD = 1;
-	private final int EDIT_CUSTOMER_BY_TAXID = 2;
-	private final int EDIT_CUSTOMER_BY_ID = 3;
-
-	private final int CHANGE_NAME = 1;
-	private final int CHANGE_PASSWORD = 2;
-	private final int CHANGE_EMAIL = 3;
-
-	private final int CREATE_DEBIT_CARD = 1;
-	private final int DELETE_DEBIT_CARD = 2;
-	private final int CREATE_CREDIT_CARD = 3;
-	private final int DELETE_CREDIT_CARD = 4;
-
-	private final int DEPOSIT_MONEY_ON_HOLDER_ACCOUNT = 1;
-	private final int DEPOSIT_MONEY_ON_SECUNDARY_ACCOUNT = 2;
-	private final int WITHDRAW_MONEY_ON_HOLDER_ACCOUNT = 3;
-	private final int WITHDRAW_MONEY_ON_SECUNDARY_ACCOUNT = 4;
-	private final int TRANSFER_MONEY = 5;
-	private final int CHECK_ACCOUNT_HISTORY = 6;
-
-	private final String MOTD = "Welcome to Rumos Digital Bank";
-	private final String TITLE = "Rumos Digital Bank";
-	private final String GOODBYE = "Thanks for using Rumos Digital Bank";
-	private final String CUSTOMER_CREATED = "Customer created!";
-	private final String CUSTOMER_DELETED = "Customer deleted!";
-	private final String CUSTOMER_NOT_FOUND = "Customer not found!";
-	private final String DATABASE_IS_FULL = "Database is full!";
-	private final String DATABASE_IS_EMPTY = "Database is empty!";
-	private final String TAXID_ALREADY_EXISTS = "Customer Tax Id already exists!";
-	private final String INVALID_OPTION = "Invalid Option!";
-	private final String INVALID_NAME_OR_PASSWORD = "Incorrect Name or Password";
-	private final String INVALID_TAXID = "Incorrect TaxID";
-	private final String INVALID_ID = "Incorrect ID";
-	private final String NO_ADD_DEBIT_CARD = "You already have a Debit Card";
-	private final String NO_ADD_CREDIT_CARD = "You already have a Credit Card";
-	private final String NO_TAKE_DEBIT_CARD = "You do not have a Debit Card";
-	private final String NO_TAKE_CREDIT_CARD = "You do not have a Credit Card";
-	private final String PREVIOUS_MENU = "Returning to previous Menu";
-	private final String NO_MONEY_TO_REMOVE = "You are removing an amount bigger than what you own";
+	private static final String TITLE = "Rumos Digital Bank";
+	private static final String INVALID_OPTION = "Invalid Option!";
+	private static final String INVALID_NAME_OR_PASSWORD = "Incorrect Name or Password";
+	private static final String PREVIOUS_MENU = "Returning to previous Menu";
+	private static final String NO_MONEY_TO_REMOVE = "You are removing an amount bigger than what you own";
 	
 	
-	ArrayList<Account> Accounts = new ArrayList<>();
-	Scanner scanner = new Scanner(System.in);
+	private ArrayList<Account> Accounts = new ArrayList<>();
+	private static Scanner scanner = new Scanner(System.in);
 	
 	
 	private static void editSecondaryAccounts() {
@@ -82,10 +45,10 @@ public class AccountService {
 
 			switch (choose) {
 			case 1:
-				addSecundaryAccount(customer);
+				addSecondaryAccount(customer);
 				break;
 			case 2:
-				deleteSecundaryAccount(customer);
+				deleteSecondaryAccount(customer);
 				break;
 			case EXIT:
 				System.out.println(PREVIOUS_MENU);
@@ -353,10 +316,58 @@ public class AccountService {
 		}
 		System.out.println("The total amount of: " + customer.getHolderAccountBalance());
 	}
+	
+	
+	/******************************************************************************
+	 * check if the name and password are correct At the moment its unused since we
+	 * need to have the index to know who is the customer
+	 * 
+	 * @return
+	 ******************************************************************************/
+	private static Customer checkNameAndPassword() {
+		System.out.println("Please write your name");
+		String name = scanner.next();
+		System.out.println("Please write your Password");
+		String password = scanner.next();
+
+		for (Customer customer : customers) {
+			if (customer == null)
+				break;
+			if (customer.getName().equals(name) && customer.getPassword().equals(password)) {
+				return customer;
+			}
+		}
+		System.err.println(INVALID_NAME_OR_PASSWORD);
+		return null;
+	}
+	
+	
+	
 
 	private static void addTAccountHistoryMovement(Customer customer, String amount) {
 		customer.getTransactionHistory().add(amount);
 	}
-
+	
+	private static void displaySecondaryAccountsMenu() {
+		System.out.println("############################ " + TITLE + " #############################");
+		System.out.println("Please choose what action to take");
+		System.out.println("0 - Return to previous Menu");
+		System.out.println("1 - Create Secondary Account");
+		System.out.println("2 - Delete Secondary Account");
+		System.out.println("###########################################################################");
+	}
+	
+	private static void displayMoneyManagementMenu() {
+		System.out.println("############################ " + TITLE + " #############################");
+		System.out.println("Please choose what action to take");
+		System.out.println("0 - Return to previous Menu");
+		System.out.println("1 - Deposit money on your Holder Account");
+		System.out.println("2 - Deposit money on your Secundary Account");
+		System.out.println("3 - Withdraw money from your Holder Account");
+		System.out.println("4 - Withdraw money from your Secondary Account");
+		System.out.println("5 - Transfer money to another account");
+		System.out.println("6 - Show history account");
+		System.out.println("###########################################################################");
+	}
 	
 }
