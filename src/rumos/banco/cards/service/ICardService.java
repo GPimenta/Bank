2e0c64a87bc0a7611566fb59fd5835adf4e1228c
GoncaloPimenta <goncalo6pimenta@gmail.com>
2021-04-18 23:46:05 +0100

    no message

--- src/rumos/banco/cards/service/ICardService.java
+++ src/rumos/banco/cards/service/ICardService.java
@@ -1,5 +1,34 @@
 package rumos.banco.cards.service;
 
+import java.util.Collection;
+
+import rumos.banco.cards.model.Card;
+import rumos.banco.cards.model.CreditCard;
+import rumos.banco.cards.model.DebitCard;
+
 public interface ICardService {
+	
+	DebitCard createDebitCard(Integer accountId, Integer customerId);
+	
+	CreditCard createCreditCard(Integer accountId, Integer customerId);
+	
+	void deleteCard(Integer cardId);
+	
+	boolean checkCardNumberAndPassword(String cardNumber, String password);
+	
+	Collection<Card> findAllCardsByCustomerId(Integer customerId);
+	
+	Collection<DebitCard> findAllDebitCardsByCustomerId(Integer customerId);
+	
+	DebitCard getDebitCardByCardId(String cardId);
+	
+	CreditCard getCreditCardByCardId(String cardId);
+	
+	Card getCardById(Integer cardId);
+	
+	
+	
+	
+	
 
 }
